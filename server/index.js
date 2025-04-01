de6446fc855d62b728aff6e2a41a8f32a9d5d329
Manthan Bire <manthansharadbire@gmail.com>
2025-04-01 19:30:52 +0530

    get api created with an empty array

--- server/index.js
+++ server/index.js
@@ -8,8 +8,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const GAMES = [];
+
 app.get("/health", (req,res)=>{
     res.status(200).json({message: "Server is Healthy"})
+});
+
+app.get("/games", (req,res)=>{
+    return res.status(200).json({
+        success:true,
+        data:GAMES,
+        meassage:"Games fetched successfully"
+    });
 });
 
 const PORT = 5002;
