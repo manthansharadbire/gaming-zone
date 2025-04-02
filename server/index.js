e25cc1dd5678dc41ee59469812b188eb81dbf31c
Manthan Bire <manthansharadbire@gmail.com>
2025-04-02 11:28:18 +0530

    MongoDB connection setup

--- server/index.js
+++ server/index.js
@@ -4,43 +4,52 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+
+    if (conn) {
+        console.log("MongoDB connected successfully")
+    }
+}
 const GAMES = [];
 
-app.get("/health", (req,res)=>{
-    res.status(200).json({message: "Server is Healthy"})
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server is Healthy" })
 });
 
-app.get("/games", (req,res)=>{
+app.get("/games", (req, res) => {
     return res.status(200).json({
-        success:true,
-        data:GAMES,
-        meassage:"Games fetched successfully"
+        success: true,
+        data: GAMES,
+        meassage: "Games fetched successfully"
     });
 });
 
-app.post("/games", (req,res)=>{
-    const {title, year , genre , thumbnail} = req.body;
+app.post("/games", (req, res) => {
+    const { title, year, genre, thumbnail } = req.body;
 
     const newGames = {
-        title, year , genre , thumbnail
+        title, year, genre, thumbnail
     }
 
     GAMES.push(newGames)
-    
+
     return res.status(201).json({
-        success:true,
-        data:GAMES,
-        message:"Game created successfully"
+        success: true,
+        data: newGames,
+        message: "Game created successfully"
     })
 })
 
-const PORT = 5002;
+const PORT = process.env.PORT || 5002;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+    connectDB();
 });
 
